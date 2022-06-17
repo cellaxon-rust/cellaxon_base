3e0347b192906ed66a56ecf520f7c456ee0db590
Sanghyo Lee <ryan@cellaxon.com>
2022-06-17 11:40:50 +0900

    잘못된 파일 이름들을 수정

--- src/tool/datetime.rs
+++ src/tool/datetime.rs
@@ -13,14 +13,14 @@ pub fn remove_symbol(string_time: &str) -> String {
 
 
 // UTC 날짜-시간으로 문자열을 생성하고, 윈도우에서 파일 이름에 허용하지 않는 기호 제거
-pub fn get_string_datetime_utc() -> String {
+pub fn get_string_date_time_utc() -> String {
     let string_time = format!("{}", chrono::offset::Utc::now());
     remove_symbol(&string_time)
 }
 
 
 // 로컬 날짜-시간으로 문자열을 생성하고, 윈도우에서 파일 이름에 허용하지 않는 기호 제거
-pub fn get_string_datetime_local() -> String {
+pub fn get_string_date_time_local() -> String {
     let string_time = format!("{}", chrono::offset::Local::now());
     remove_symbol(&string_time)
 }
