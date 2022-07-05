d7d59ff390b7ea7c7b0cc2f9ca025b6ebbc59616
Sanghyo Lee <ryan@cellaxon.com>
2022-07-05 12:55:13 +0900

    ticker에 카운트 추가

--- src/tool/ticker.rs
+++ src/tool/ticker.rs
@@ -3,6 +3,7 @@ use std::time::Instant;
 
 pub struct Ticker
 {
+    count: u128,
     time_start: Instant,
     time_next: u128,
     time_interval: u32,
@@ -15,8 +16,9 @@ impl Ticker
     {
         Ticker
         {
+            count: 0,
             time_start: Instant::now(),
-            time_next: 0,
+            time_next: 100,
             time_interval: 100,
         }
     }
@@ -24,12 +26,12 @@ impl Ticker
 
     pub fn new(time_interval: u32) -> Ticker
     {
-        Ticker
-        {
-            time_start: Instant::now(),
-            time_next: time_interval as u128,
-            time_interval: time_interval,
-        }
+        let mut ticker = Ticker::empty();
+
+        ticker.time_next = time_interval as u128;
+        ticker.time_interval = time_interval;
+
+        ticker
     }
 
 
@@ -44,12 +46,18 @@ impl Ticker
         let time_now = self.time_start.elapsed().as_millis();
         if time_now > self.time_next
         {
+            self.count += 1;
             self.time_next = self.time_next + self.time_interval as u128;
 
             return true;
         }
 
         return false;
+    }
+
+    pub fn get_count(&self) -> u128
+    {
+        self.count
     }
 }
 
